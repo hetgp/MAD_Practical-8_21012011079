2ea7dc565b9eba1c1dd6479f2668218ece339b40
student <student@gnu.ac.in>
2023-09-28 08:16:10 +0530

    al

--- app/src/main/java/com/example/mad_practical_8_21012011079/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_8_21012011079/MainActivity.kt
@@ -8,8 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextClock
+import android.widget.Toast
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
+import java.text.SimpleDateFormat
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +27,14 @@ class MainActivity : AppCompatActivity() {
         card.visibility = View.GONE
 
         addAlarm.setOnClickListener {
-            TimePickerDialog(this, {tp, hour, minute -> setAlarmTime(hour,minute)}, Calendar.HOUR, Calendar.MINUTE,false).show()
+            TimePickerDialog(this, {tp, hour, minute -> setAlarmTime(hour,minute)}, Calendar.getInstance().get(Calendar.HOUR), Calendar.getInstance().get(Calendar.MINUTE), false).show()
+            card.visibility = View.VISIBLE
+        }
+
+        val cancelAlarm : MaterialButton = findViewById(R.id.cancel)
+        cancelAlarm.setOnClickListener {
+            stop()
+            card.visibility = View.GONE
         }
     }
 
@@ -47,7 +57,7 @@ class MainActivity : AppCompatActivity() {
     fun setAlarm(militime : Long, action: String) {
         val intentalarm = Intent(applicationContext, AlarmBroadcastReceiver::class.java)
         intentalarm.putExtra(AlarmBroadcastReceiver.ALARMKEY,action)
-        val pendingintent = PendingIntent.getBroadcast(applicationContext,4356, intentalarm, PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingintent = PendingIntent.getBroadcast(applicationContext,4356, intentalarm, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
         val  alarmmanager = getSystemService(ALARM_SERVICE) as AlarmManager
 
         if (action == AlarmBroadcastReceiver.ALARMSTART) {
